15d99b706b2e860bb4672ad621c6efdf162719c3
Lexuil <ojumanab@unal.edu.co>
2023-03-01 15:32:12 -0500

    gitignore .docusaurus folder. first version.

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/oscar/Documents/chatbot-ecci/chatbot_uecci_user_doc/node_modules/infima/dist/css/default/default.css'),
-  require('/home/oscar/Documents/chatbot-ecci/chatbot_uecci_user_doc/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/oscar/Documents/chatbot-ecci/chatbot_uecci_user_doc/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/oscar/Documents/chatbot-ecci/chatbot_uecci_user_doc/src/css/custom.css'),
-];
